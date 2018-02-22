e3d14eff447c8ba8366ebda97b86e6896c518c92
admin <admin@1DAM-O01>
2018-02-22 11:32:48 +0100

    subida del proyecto al repositorio local. 22/02/2018

--- src/proyectogit/ProyectoGit.java
+++ src/proyectogit/ProyectoGit.java
@@ -15,7 +15,7 @@ public class ProyectoGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola");
+        System.out.println("Hola adios");
               
     }
     
